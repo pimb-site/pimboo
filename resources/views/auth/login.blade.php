a5c01236f81dd914772c7cd0372ad76622323ed5
DieselDenia <dykaandden@gmail.com>
2017-02-16 09:24:11 +0200

    another some work

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,27 @@
-@extends('app')
+@extends('page')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="/auth/login">
+		<div class="login">
+			<div class="wrap">
+				<div class="title">LOGIN</div>
+				<div class="form">
+					<form class="form-horizontal" role="form" method="POST" action="/auth/login">	
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+						<input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
+						<input type="password" name="password" placeholder="Password">
+						<label><input class="checkbox" type="checkbox" name="remember"><span class="checkbox-custom"></span><span class="label">Remember me</span></label>
+						@if (count($errors) > 0)
+							<div class="alerts">
+								@foreach ($errors->all() as $error)
+										<div class="alert">{{ $error }}</div>
+									@endforeach
+								</ul>
 							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-									Login
-								</button>
-
-								<a href="/password/email">Forgot Your Password?</a>
-							</div>
-						</div>
+						@endif
+						<button>LOGIN</button>
+						<a href="/password/email">Forgot your password?</a>
 					</form>
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
 @endsection
