0f15ca6feeae8c8f943990e150939a8052957da7
DieselDenia <dykaandden@gmail.com>
2017-05-08 06:18:26 +0300

    ads update

--- app/Http/Controllers/ToolsController.php
+++ app/Http/Controllers/ToolsController.php
@@ -2,6 +2,7 @@
 use Illuminate\View\View;
 use App\PostView;
 use Auth;
+use Input;
 use Illuminate\Http\Request;
 use App\User;
 
@@ -15,8 +16,8 @@ class ToolsController extends Controller
     }
 
     public function viewStories() {
-
         $contentflip = \DB::select('select * from posts where type = "story" and isDraft = "publish"');
+
         return view('view_stories', ['contentflip' => $contentflip]);
     }
 
@@ -24,6 +25,7 @@ class ToolsController extends Controller
     public function viewID($id, Request $request) {
     	$view = new PostView;
     	$view->post_id = $id;
+ 
     	if (!Auth::guest()) {
     		$view->user_id = Auth::user()->id;
     	}
@@ -34,14 +36,19 @@ class ToolsController extends Controller
         $content = \DB::select('select * from posts where id = ? and isDraft = ?', [$id, 'publish']);
         $content = $content[0];
         $user_model = User::where([ ['id', '=', $content->user_id] ])->first();
+
         if (isset($user_model->id)) {
             $user_name = $user_model->name;
         } else {
             $user_name = 'Unknown';
         }
+
         $ads = \DB::select('select * from settings where setting = "ads"');
         $ads = unserialize($ads[0]->value);
-        print_r($ads);
+
+        foreach ($ads as &$ad) {
+            $ad['href'] = $ad['href'].'&s2='.$content->user_id.'_'.Input::get('sub');
+        }
         return view('tools.'.$content->type, ['body_class' => 'view '.$content->type, 'content' => $content, 'name' => $content->type, 'user_name' => $user_name, 'source_link' => '', 'ads' => $ads ]);
     }
 	
