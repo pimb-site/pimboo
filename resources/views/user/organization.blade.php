612627fe1415d0fc6d7d803f11c01a76d9dc1564
forcewarden <forcewarden@gmail.com>
2017-06-21 18:40:32 +0300

    fixed errors
    removed time on home page; fixed page organization;

--- resources/views/user/organization.blade.php
+++ resources/views/user/organization.blade.php
@@ -52,9 +52,9 @@
 							$i = 1;
 						}
 						?>
-						height: <?=$i;?>px;
-						margin-bottom: <?=50*$l?>px;
-						margin-top: <?=50*$f?>px;
+						height: <?php echo $i; ?>px;
+						margin-bottom: <?php echo 50*$l; ?>px;
+						margin-top: <?php echo 50*$f; ?>px;
 						"
 					"></div>
 					<div id="ref-tree-first">
@@ -66,7 +66,7 @@
 										<div class="line" style="border-top: 1px solid black; width: 100%;"></div>
 									</td>
 									<td height="100" valign="middle">
-										<div class="user" data-trigger="hover" data-content='<?=$content?>' data-html="true" data-placement="right" id="user-<?=$user_f_l->id?>" style='z-index: 1000;'>
+										<div class="user" data-trigger="hover" data-content='<?php echo $content; ?>' data-html="true" data-placement="right" id="user-<?php echo $user_f_l->id; ?>" style='z-index: 1000;'>
 											@if (empty($user_f_l->photo))
 											<img width="84px" height="84px" src="/img/header_default_photo.png" />
 										@else
@@ -84,7 +84,7 @@
 									</td>
 								</tr>
 							</table> 
-						<? endforeach; ?>
+						<?php endforeach; ?>
 					</div>
 					<div id="ref-tree-second">
 						
@@ -92,7 +92,7 @@
 							<div class="user-group">
 								<div class="vertical-ref-line"
 									style="
-									<? $c = count($my_org_users_second_level[$user_f_l->id]); 
+									<?php $c = count($my_org_users_second_level[$user_f_l->id]); 
 									if ($c*100-100 <= 0) {
 										$c = 1;
 									} else {
@@ -100,9 +100,9 @@
 									}
 									?>
 									
-									height: <?=$c;?>px;
-									margin-bottom: <?=50;?>px;
-									margin-top: <?=50;?>px;
+									height: <?php echo $c; ?>px;
+									margin-bottom: <php echo 50;?>px;
+									margin-top: <?php echo 50; ?>px;
 									" ></div>
 								<table cellpadding="0" cellspacing="0" border="0" style="height: <?php  if(count($my_org_users_second_level[$user_f_l->id]) != 0) { echo count($my_org_users_second_level[$user_f_l->id])*100; } else { echo 100; } ?>px">
 									<?php foreach($my_org_users_second_level[$user_f_l->id] as $user_s_l) : ?>
@@ -112,7 +112,7 @@
 											<div class="line" style="border-top: 1px solid black; width: 100%;"></div>
 										</td>
 										<td height="100" valign="middle">
-											<div class="user" data-trigger="hover" data-content='<?=$content?>' data-html="true" data-placement="right" id="user-<?=$user_s_l->id?>" style='z-index: 1000;'>
+											<div class="user" data-trigger="hover" data-content='<?php echo $content; ?>' data-html="true" data-placement="right" id="user-<?php echo $user_s_l->id; ?>" style='z-index: 1000;'>
 												@if (empty($user_s_l->photo))
 													<img width="84px" height="84px" src="/img/header_default_photo.png" />
 												@else
@@ -124,10 +124,10 @@
 											</div>
 										</td>
 									</tr>
-									<? endforeach; ?>
+									<?php endforeach; ?>
 								</table> 
 							</div>
-						<? endforeach; ?>
+						<?php endforeach; ?>
 					</div>
 				</div>
 			</div>
